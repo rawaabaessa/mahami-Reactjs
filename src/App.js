1209fc02e56a39781fa88f8558c3369174eb47fb
rawaabaessa <rawaabaessa@gmail.com>
2024-07-02 17:27:18 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/Login";
 import Managment from "./components/Managment";
 import Signup from "./components/Signup";
 import ProtectedRoutes from "./services/ProtectedRoutes";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { TaskContext } from "./contexts/taskContext";
 import { useState } from "react";
 import { Tasks } from "./data/Tasks";
@@ -12,22 +12,20 @@ import { Tasks } from "./data/Tasks";
 function App() {
   const [tasks, setTasks] = useState(Tasks);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<ProtectedRoutes />}>
-          <Route
-            path="/dashboard"
-            element={
-              <TaskContext.Provider value={{ tasks, setTasks }}>
-                <Managment />
-              </TaskContext.Provider>
-            }
-          />
-        </Route>
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<ProtectedRoutes />}>
+        <Route
+          path="/dashboard"
+          element={
+            <TaskContext.Provider value={{ tasks, setTasks }}>
+              <Managment />
+            </TaskContext.Provider>
+          }
+        />
+      </Route>
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
